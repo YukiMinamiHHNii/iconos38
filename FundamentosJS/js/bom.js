5fc6ecc81cbb7a0153c002ca708cab5f75bb459a
jonmircha <jonmircha@gmail.com>
2018-10-27 15:49:59 -0500

    Clase 1 Programación Avanzada

--- FundamentosJS/js/bom.js
+++ FundamentosJS/js/bom.js
@@ -92,21 +92,111 @@
       '\n',
       isMobile.any()
     )
+
+    bUserAgent.textContent = ua
+    bPlatform.textContent = (isMobile.any()) ? isMobile.any() : isDesktop.any()
+    bBrowser.textContent = isBrowser.any()
   })
 })(document, window, navigator, console.log);
 
 
 
 ((d, w, n, c) => {
+  const networkStatus = (status) => {
+    const div = d.createElement('div'),
+      h2Status = d.getElementById('network-status')
 
+    h2Status.insertAdjacentElement('afterend', div)
+
+    if (status) {
+      div.textContent = 'Conexión Reestablecida'
+      div.classList.add('online')
+      div.classList.remove('offline')
+    } else {
+      div.textContent = 'Conexión Perdida'
+      div.classList.remove('online')
+      div.classList.add('offline')
+    }
+
+    setTimeout(() => d.body.removeChild(div), 2000)
+  }
+
+  d.addEventListener('DOMContentLoaded', e => {
+    c('********** Detección de la conexión **********')
+
+    if (n.onLine) {
+      c('Sitio online al cargar el DOM')
+    } else {
+      c('Sitio offline al cargar el DOM')
+    }
+  })
+
+  w.addEventListener('online', e => {
+    c('Sitio online')
+    networkStatus(n.onLine)
+  })
+
+  w.addEventListener('offline', e => {
+    c('Sitio offline')
+    networkStatus(n.onLine)
+  })
 })(document, window, navigator, console.log);
 
 ((d, w, n, c) => {
+  d.addEventListener('DOMContentLoaded', e => {
+    c('********** Webcam **********')
 
+    const video = d.getElementById('webcam')
+
+    c(n.mediaDevices.getUserMedia)
+
+    if (n.mediaDevices.getUserMedia) {
+      n.mediaDevices.getUserMedia({ video: true, audio: true })
+        .then(stream => {
+          video.srcObject = stream
+          video.play()
+        })
+        .catch(err => c(`Sucedio el siguiente error: ${err}`))
+    }
+  })
 })(document, window, navigator, console.log);
 
 
 
 ((d, w, n, c) => {
+  d.addEventListener('DOMContentLoaded', e => {
+    c('********** Geolocalización **********')
 
+    const bLatitude = d.getElementById('latitude'),
+      bLongitude = d.getElementById('longitude'),
+      bAccuracy = d.getElementById('accuracy'),
+      options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+      }
+
+    const success = position => {
+      let coords = position.coords
+      c(position)
+
+      bLatitude.textContent = coords.latitude
+      bLongitude.textContent = coords.longitude
+      bAccuracy.textContent = coords.accuracy
+    }
+
+    const error = err => c(`Error ${err.code}: ${err.message}`)
+
+    n.geolocation.getCurrentPosition(success, error, options)
+  })
+
+  d.addEventListener('click', e => {
+    if (e.target.matches('#map')) {
+      let latitude = d.getElementById('latitude').textContent,
+        longitude = d.getElementById('longitude').textContent,
+        zoom = '20z'
+
+      w.open(`https://www.google.com/maps/@${latitude},${longitude},${zoom}`)
+    }
+  })
 })(document, window, navigator, console.log);
