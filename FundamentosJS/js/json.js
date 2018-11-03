036561a0c52ff78192ceb9c88c6fd62f775f149f
jonmircha <jonmircha@gmail.com>
2018-11-03 14:33:05 -0600

    Clase 2 Programación Avanzada

--- FundamentosJS/js/json.js
+++ FundamentosJS/js/json.js
@@ -1,4 +1,40 @@
 ;
-((d, w, n, c) => {
+((d, w, n, c, j) => {
+  c('********** JSON **********')
 
-})(document, window, navigator, console.log);
+  c('********** JSON.stringify() **********')
+
+  c(
+    j.stringify({}),
+    '\n',
+    j.stringify(19),
+    '\n',
+    j.stringify(true),
+    '\n',
+    j.stringify('foo'),
+    '\n',
+    j.stringify([1, 'hola', false]),
+    '\n',
+    j.stringify({ x: 5, y: 6 }),
+    '\n',
+    j.stringify(null)
+  )
+
+  c('********** JSON.parse() **********')
+
+  c(
+    j.parse('{}'),
+    '\n',
+    j.parse('19'),
+    '\n',
+    j.parse('true'),
+    '\n',
+    j.parse('"foo"'),
+    '\n',
+    j.parse('[1, "hola", false]'),
+    '\n',
+    j.parse('{"x": "5", "y": "6"}'),
+    '\n',
+    j.parse('null')
+  )
+})(document, window, navigator, console.log, JSON);
