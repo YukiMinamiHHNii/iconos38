962f664e0d94c30feea0ad7f877e85b92b823c23
jonmircha <jonmircha@gmail.com>
2018-11-03 15:46:12 -0600

    Clase 2 Programación Avanzada

--- FundamentosJS/js/ajax.js
+++ FundamentosJS/js/ajax.js
@@ -1,4 +1,20 @@
 ;
 ((d, w, n, c) => {
+  fetch('./js/movies.json')
+    .then(res => {
+      c(res)
+    })
+    .catch(err => {
+      c(err)
+    })
+})(document, window, navigator, console.log);
+
+
+((d, w, n, c) => {
+
+})(document, window, navigator, console.log);
+
+
+((d, w, n, c) => {
 
 })(document, window, navigator, console.log);
